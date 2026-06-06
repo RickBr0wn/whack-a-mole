0bc2b21d8f6f5ea09197f0eb4ac2f221d74d0e68
RickBr0wn <ricky.brown.00@gmail.com>
2026-06-06 17:55:12 +0100

    Initial Commit

---
+++ whack-a-mole/whack_a_moleApp.swift
@@ -0,0 +1,17 @@
+//
+//  whack_a_moleApp.swift
+//  whack-a-mole
+//
+//  Created by Richard Brown on 06/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct whack_a_moleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
